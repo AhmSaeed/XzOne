f252603badeb0479097ceee63f2414d2e5ec47bc
Ahmad Khattab <akhattab03@gmail.com>
2020-12-26 22:57:36 +0200

    adding sign up view

--- src/controllers/SignInController.java
+++ src/controllers/SignInController.java
@@ -5,14 +5,20 @@
  */
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 
 /**
  * FXML Controller class
@@ -40,6 +46,16 @@ public class SignInController implements Initializable {
 
     @FXML
     private void goToSignUP(ActionEvent event) {
+        Parent root = null;
+        
+        try {
+            root = FXMLLoader.load(getClass().getResource("/views/SignUpView.fxml"));
+        } catch (IOException ex) {
+            Logger.getLogger(SignInController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        ((BorderPane)signUpBtn.getScene().getRoot()).setCenter(root);
+        
     }
     
 }
